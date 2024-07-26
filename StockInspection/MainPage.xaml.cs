a2f91c6343c1085f9e3ca6736cada07883b06156
maria fernanda <mariafernandalibano345@gmail.com>
2024-07-26 08:58:09 -0300

    arrumando xaml

---
+++ StockInspection/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace StockInspection
+{
+    public partial class MainPage 
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        // Add event handlers if needed, for example:
+        private void OnClientesClicked(object sender, EventArgs e)
+        {
+            // Handle the button click
+        }
+
+        private void OnProdutosClicked(object sender, EventArgs e)
+        {
+            // Handle the button click
+        }
+
+        private void OnEstoqueClicked(object sender, EventArgs e)
+        {
+            // Handle the button click
+        }
+
+        private void OnVendasClicked(object sender, EventArgs e)
+        {
+            // Handle the button click
+        }
+    }
+}
+
